bbd08fad98b8df16b3cbcb163f557054afc75441
Juanes0907 <juanesbanguero0907@gmail.com>
2024-12-06 14:46:51 -0500

    Correción ejercicios

--- PHPADVANCE/Files.php
+++ PHPADVANCE/Files.php
@@ -1,10 +1,10 @@
 <?php 
     $file = fopen("archivo.txt","r") or die( "No se puede abrir el archivo");
-    //echo fread($file,filesize("archivo.txt"));
-    //fclose($file);
-    //$nofile = fopen("nofile.txt","r") or die( "No existe el archivo");
-    //echo fread($nofile,filesize("nofile.txt"));
-    //fclose($nofile);
+    echo fread($file,filesize("archivo.txt"));
+    fclose($file);
+    $nofile = fopen("nofile.txt","r") or die( "No existe el archivo");
+    echo fread($nofile,filesize("nofile.txt"));
+    fclose($nofile);
 
     echo fgets($file);
     fclose($file);
